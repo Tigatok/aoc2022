2633936bc1eb912f9b13b33c19da9f1d74aae9c4
Tyler Marshall <tyler@dovetaildigital.ca>
2022-12-06 05:57:27 -0800

    Clean up clean up everybody everywhere.
    Clean up clean up everybody do your share.
    Clean up clean up everybody everywhere.
    Clean up clean up everybody do your share.
    Clean up clean up everybody everywhere.
    Clean up clean up everybody do your share

--- day6/day6.py
+++ day6/day6.py
@@ -1,15 +1,21 @@
-day6 = open("./day6.txt", "r")
-day6 = day6.read()
+def main():
+    day6 = open("./day6.txt", "r")
+    day6 = day6.read()
+    print(readMessage(day6, 4))
+    print(readMessage(day6, 14))
 
 def checkUnique(st):
     return len(set(st)) == len(st)
 
-totalString = ''
-index = 0
-while index <= len(day6):
-    totalString = totalString + day6[index]
-    chunk = totalString[index-14:index]
-    if len(chunk) >= 14 and checkUnique(chunk):
-        break;
-    index = index + 1
-print(index)
+def readMessage(string, cap):
+    totalString = ''
+    index = 0
+    while index <= len(string):
+        totalString = totalString + string[index]
+        chunk = totalString[index-cap:index]
+        if len(chunk) >= cap and checkUnique(chunk):
+            return index
+        index = index + 1
+
+if __name__ == "__main__":
+    main()
